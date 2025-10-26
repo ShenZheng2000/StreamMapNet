c4f8d8864aa49442b769c7769d4343bcb6e192ba
Shen Zheng <shenzhen@trinity-2-5.eth>
2025-10-25 22:38:14 -0400

    10_25_exps: FIX gt cache hardcode (mAP=0 for map change)

---
+++ plugin/configs/mapchange_608_60x30_30e.py
@@ -0,0 +1,30 @@
+_base_ = [
+    'av2_newsplit_608_60x30_30e.py'
+]
+
+# overwrite PKL paths everywhere
+new_val_pkl = '/scratch/shenzhen/datasets/mapchange/av2_map_infos_val.pkl'
+new_train_pkl = '/scratch/shenzhen/datasets/mapchange/av2_map_infos_train.pkl'
+
+data = dict(
+    train=dict(
+        ann_file=new_train_pkl,
+    ),
+    val=dict(
+        ann_file=new_val_pkl,
+        eval_config=dict(
+            ann_file=new_val_pkl,   # <--- must override here too
+        ),
+    ),
+    test=dict(
+        ann_file=new_val_pkl,
+        eval_config=dict(
+            ann_file=new_val_pkl,   # <--- must override here too
+        ),
+    ),
+)
+
+# also override top-level eval_config (for safety)
+eval_config = dict(
+    ann_file=new_val_pkl,
+)
--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
-conda activate maptracker
-pip install ipython
+# conda activate maptracker
+# pip install ipython
 
 # Training
-bash tools/dist_train.sh plugin/configs/argoverse2_geosplit_608_60x30_30e.py 8
-bash tools/dist_train.sh plugin/configs/nusc_baseline_480_60x30_30e.py 8
+# bash tools/dist_train.sh plugin/configs/argoverse2_geosplit_608_60x30_30e.py 8
+# bash tools/dist_train.sh plugin/configs/nusc_baseline_480_60x30_30e.py 8
+# bash tools/dist_train.sh plugin/configs/mapchange_geosplit_608_60x30_30e.py 8
+# bash tools/dist_train.sh plugin/configs/mapchange_608_60x30_30e.py 8
 
 # Evaluation
-bash tools/dist_test.sh plugin/configs/argoverse2_geosplit_608_60x30_30e.py work_dirs/argoverse2_geosplit_608_60x30_30e/latest.pth 8 --eval
-bash tools/dist_test.sh plugin/configs/nusc_baseline_480_60x30_30e.py work_dirs/nusc_baseline_480_60x30_30e/latest.pth 8 --eval
+# bash tools/dist_test.sh plugin/configs/argoverse2_geosplit_608_60x30_30e.py work_dirs/argoverse2_geosplit_608_60x30_30e/latest.pth 8 --eval
+# bash tools/dist_test.sh plugin/configs/nusc_baseline_480_60x30_30e.py work_dirs/nusc_baseline_480_60x30_30e/latest.pth 8 --eval
+# bash tools/dist_test.sh plugin/configs/mapchange_geosplit_608_60x30_30e.py work_dirs/mapchange_geosplit_608_60x30_30e/latest.pth 8 --eval
